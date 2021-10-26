7a47d0d77870d612c763c15662ae6027cc9c1090
James <jrvollmer@wisc.edu>
2021-10-26 16:51:42 -0500

    Dashboard base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from "./Components/Dashboard/Dashboard.js";
+
+function App() {
+  return (
+      <div className="App">
+        <Dashboard/>
+      </div>
+  );
+}
+
+export default App;
