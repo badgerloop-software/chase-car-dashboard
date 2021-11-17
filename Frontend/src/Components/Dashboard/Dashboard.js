8d4d25a24659066b345e424b9f02f1da925d47ec
zick2 <34427350+zick2@users.noreply.github.com>
2021-11-16 18:58:51 -0600

    main UI skeleton (less detailed)

--- Frontend/src/Components/Dashboard/Dashboard.js
+++ Frontend/src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import { Box, Heading, Text } from "@chakra-ui/layout";
+import "./Dashboard.css"
+import { Box, Heading, Text, Flex, Center, Spacer, Square, GridItem, Grid } from "@chakra-ui/layout";
 import React, { useState, useLayoutEffect } from "react";
 
 export default function Dashboard(props) {
@@ -26,21 +27,51 @@ export default function Dashboard(props) {
 
   return (
     <Box>
-      <Heading>Dashboard</Heading>
-      <Text>Speed: {state.data?.speed}</Text>
-      <Text>Power: {state.data?.power}</Text>
-      <Text>Charge: {state.data?.charge}</Text>
-      <Text>netPower: {state.data?.netPower}</Text>
-      <Text>motorPower: {state.data?.motorPower}</Text>
-      <Text>milesLeft: {state.data?.milesLeft}</Text>
-      <Text>batteryTemp: {state.data?.batteryTemp}</Text>
-      <Text>motorTemp: {state.data?.motorTemp}</Text>
-      <Text>motorControllerTemp: {state.data?.motorControllerTemp}</Text>
-      <Text>frontLeftTP: {state.data?.frontLeftTP}</Text>
-      <Text>frontRightTP: {state.data?.frontRightTP}</Text>
-      <Text>backLeftTP: {state.data?.backLeftTP}</Text>
-      <Text>backRightTP: {state.data?.backRightTP}</Text>
-      <Text>state: {state.data?.state}</Text>
+      {/* <Heading>Dashboard</Heading> */}
+      {/* <Box className="dashborad-container" >
+        <Text>Speed: {state.data?.speed}</Text>
+        <Text>Power: {state.data?.power}</Text>
+        <Text>Charge: {state.data?.charge}</Text>
+        <Text>netPower: {state.data?.netPower}</Text>
+        <Text>motorPower: {state.data?.motorPower}</Text>
+        <Text>milesLeft: {state.data?.milesLeft}</Text>
+        <Text>batteryTemp: {state.data?.batteryTemp}</Text>
+        <Text>motorTemp: {state.data?.motorTemp}</Text>
+        <Text>motorControllerTemp: {state.data?.motorControllerTemp}</Text>
+        <Text>frontLeftTP: {state.data?.frontLeftTP}</Text>
+        <Text>frontRightTP: {state.data?.frontRightTP}</Text>
+        <Text>backLeftTP: {state.data?.backLeftTP}</Text>
+        <Text>backRightTP: {state.data?.backRightTP}</Text>
+        <Text>state: {state.data?.state}</Text>
+      </Box> */}
+      <Grid
+        h="100vh"
+        templateRows="repeat(3, 1fr)"
+        templateColumns="repeat(5, 1fr)"
+        gap={1}
+        style={{ border: "1px solid red" }}
+      >
+        <GridItem rowSpan={3} colSpan={1} bg="blue.100" >
+          <Grid
+            h="100%"
+            templateRows="repeat(3, 1fr)"
+            templateColumns="repeat(1, 1fr)"
+            gap={1}
+          >
+            <GridItem colSpan={1} bg="blue.200" />
+            <GridItem colSpan={1} bg="blue.200" />
+            <GridItem colSpan={1} bg="blue.200" />
+          </Grid>
+        </GridItem>
+
+        <GridItem colSpan={4} bg="blue.100" >
+        </GridItem>
+        <GridItem colSpan={4} bg="blue.100" >
+        </GridItem>
+        <GridItem colSpan={4} bg="blue.100" >
+        </GridItem>
+      </Grid>
+
     </Box>
   );
 }
