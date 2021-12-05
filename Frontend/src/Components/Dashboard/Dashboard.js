7bd27709790a8318e8a07f0c556d48b19be7897d
James <jrvollmer@wisc.edu>
2021-12-05 02:14:20 -0600

    Prevent duplicate graphs and data views

--- Frontend/src/Components/Dashboard/Dashboard.js
+++ Frontend/src/Components/Dashboard/Dashboard.js
@@ -35,15 +35,33 @@ export default function Dashboard(props) {
 
   //------------------- Choosing data views using Select components -------------------
 
-  const [dataView, setDataView] = React.useState("");
+  const [dataView1, setDataView1] = React.useState("");
   const [dataView2, setDataView2] = React.useState("");
+
   // Update the value indicating which data view to display when an option is selected
   const selectDataView = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(event.target.id === "dataViewSelect1")
-        setDataView(event.target.value);
-    else if(event.target.id === "dataViewSelect2")
-        setDataView2(event.target.value);
+    if(event.target.id === "dataViewSelect1") {
+      // Avoid duplicate data views, unless they are both empty
+      if((event.target.value === document.getElementById("dataViewSelect2").value)
+         && (event.target.value !== "")) {
+        // If trying to switch to a data view that is already being displayed in the other
+        // section, switch the data views in this section and the other one
+        setDataView2(dataView1);
+        console.log(event.target.value.toString());
+      }
+      setDataView1(event.target.value);
+    } else if(event.target.id === "dataViewSelect2") {
+      // Avoid duplicate data views, unless they are both empty
+      if((event.target.value === document.getElementById("dataViewSelect1").value)
+          && (event.target.value !== "")) {
+        // If trying to switch to a data view that is already being displayed in the other
+        // section, switch the data views in this section and the other one
+        setDataView1(dataView2);
+      }
+      setDataView2(event.target.value);
+    }
   };
+
   // Choose the data view to return/display based on the given option
   const switchDataView = (optionValue) => {
     if(optionValue === "general") {
@@ -59,18 +77,66 @@ export default function Dashboard(props) {
 
   //------------------- Choosing graphs using Select components -------------------
 
-  const [graph, setGraph] = React.useState("");
+  const [graph1, setGraph1] = React.useState("");
   const [graph2, setGraph2] = React.useState("");
   const [graph3, setGraph3] = React.useState("");
+
   // Update the value indicating which graph to display when an option is selected
   const selectGraph = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(event.target.id === "graphSelect1")
-      setGraph(event.target.value);
-    else if(event.target.id === "graphSelect2")
+    if(event.target.id === "graphSelect1") {
+      // Avoid duplicate graphs, unless they are both empty
+      if(event.target.value !== "") {
+        // If trying to switch to a graph that is already being displayed in another
+        // section, switch the graphs in this section and the other one
+        switch(event.target.value) {
+          case(document.getElementById("graphSelect2").value):
+            setGraph2(graph1);
+            break;
+          case(document.getElementById("graphSelect3").value):
+            setGraph3(graph1);
+            break;
+          default:
+            break;
+        }
+      }
+      setGraph1(event.target.value);
+    } else if(event.target.id === "graphSelect2") {
+      // Avoid duplicate graphs, unless they are both empty
+      if(event.target.value !== "") {
+        // If trying to switch to a graph that is already being displayed in another
+        // section, switch the graphs in this section and the other one
+        switch(event.target.value) {
+          case(document.getElementById("graphSelect1").value):
+            setGraph1(graph2);
+            break;
+          case(document.getElementById("graphSelect3").value):
+            setGraph3(graph2);
+            break;
+          default:
+            break;
+        }
+      }
       setGraph2(event.target.value);
-    else if(event.target.id === "graphSelect3")
+    } else if(event.target.id === "graphSelect3") {
+      // Avoid duplicate graphs, unless they are both empty
+      if(event.target.value !== "") {
+        // If trying to switch to a graph that is already being displayed in another
+        // section, switch the graphs in this section and the other one
+        switch(event.target.value) {
+          case(document.getElementById("graphSelect1").value):
+            setGraph1(graph3);
+            break;
+          case(document.getElementById("graphSelect2").value):
+            setGraph2(graph3);
+            break;
+          default:
+            break;
+        }
+      }
       setGraph3(event.target.value);
+    }
   };
+
   // Choose the graph to return/display based on the given option
   const switchGraph = (optionValue) => {
     if(optionValue === "battery") {
@@ -123,14 +189,14 @@ export default function Dashboard(props) {
                   variant="filled"
                   bgColor="grey.300"
                   placeholder="Select option"
-                  value={dataView}
+                  value={dataView1}
                   onChange={selectDataView}
               >
                 <option value="general">General Data</option>
                 <option value="battery">Battery Cells</option>
                 <option value="minimap">Minimap</option>
               </Select>
-              {(switchDataView)(dataView)}
+              {(switchDataView)(dataView1)}
             </VStack>
             <VStack
                 align="stretch"
@@ -177,14 +243,14 @@ export default function Dashboard(props) {
                   variant="filled"
                   bgColor="grey.300"
                   placeholder="Select option"
-                  value={graph}
+                  value={graph1}
                   onChange={selectGraph}
               >
                 <option value="battery">Battery</option>
                 <option value="power">Power</option>
                 <option value="temperature">Temperature</option>
               </Select>
-              {(switchGraph)(graph)}
+              {(switchGraph)(graph1)}
             </VStack>
             <VStack
                 h="100%"
