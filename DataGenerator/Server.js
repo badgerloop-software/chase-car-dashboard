3329d27a3da8d959722788320d323551b18ca8b7
James <jrvollmer@wisc.edu>
2021-12-05 13:53:44 -0600

    Pseudocode for adding more appropriate data to Buffer

--- DataGenerator/Server.js
+++ DataGenerator/Server.js
@@ -31,6 +31,12 @@ const server = net.createServer(function(socket) {
 	console.log("New connection :)")
 
 	setInterval(()=>{
+		/*Psuedocode: Automatically populating a Buffer of the correct length with groups of bytes in the correct order
+		              (speed, charge, solarPower, etc.) and formats (float, char, uint8, etc.) that represent random values from 0-100
+		for(const property in DATA_FORMAT) {
+			randomVal = (i++)%101;
+			buf1.appendByteGroup(randomVal.toByteGroup(DATA_FORMAT[property][1]));
+		}*/
 		buf1 = Buffer.alloc(bytes, (i++)%101); // 52 bytes (currently) in byte array format; wrap values so they don't exceed 100
 		socket.write(buf1);
 		// socket.pipe(socket);
