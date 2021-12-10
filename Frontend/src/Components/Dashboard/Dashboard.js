593a64aac04ecd0d80552aaac9193c1c2b40e71f
Gui Guy <jedandeganwang1999@gmail.com>
2021-12-09 20:47:33 -0600

    Changed options declaration to the Dashboard.js file

--- Frontend/src/Components/Dashboard/Dashboard.js
+++ Frontend/src/Components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React, { useState, useLayoutEffect } from "react";
 import FaultsView from "../Faults/FaultsView";
 import DataViewOptions from "./DataViewOptions";
 import DataView from "../GeneralData/DataView";
-import BatteryCells from "../BatteryCells/BatteryCells"
+import BatteryCells from "../BatteryCells/BatteryCells";
 import MiniMap from "../MiniMap/MiniMap";
 import GraphOptions from "./GraphOptions";
 import BatteryGraph from "../Graph/BatteryGraph";
@@ -41,21 +41,27 @@ export default function Dashboard(props) {
   const [dataView2, setDataView2] = React.useState("");
 
   // Update the value indicating which data view to display when an option is selected
-  const selectDataView = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(event.target.id === "dataViewSelect1") {
+  const selectDataView = (event) => {
+    if (event.target.id === "dataViewSelect1") {
       // Avoid duplicate data views, unless they are both empty
-      if((event.target.value === document.getElementById("dataViewSelect2").value)
-         && (event.target.value !== "")) {
+      if (
+        event.target.value ===
+          document.getElementById("dataViewSelect2").value &&
+        event.target.value !== ""
+      ) {
         // If trying to switch to a data view that is already being displayed in the other
         // section, switch the data views in this section and the other one
         setDataView2(dataView1);
         console.log(event.target.value.toString());
       }
       setDataView1(event.target.value);
-    } else if(event.target.id === "dataViewSelect2") {
+    } else if (event.target.id === "dataViewSelect2") {
       // Avoid duplicate data views, unless they are both empty
-      if((event.target.value === document.getElementById("dataViewSelect1").value)
-          && (event.target.value !== "")) {
+      if (
+        event.target.value ===
+          document.getElementById("dataViewSelect1").value &&
+        event.target.value !== ""
+      ) {
         // If trying to switch to a data view that is already being displayed in the other
         // section, switch the data views in this section and the other one
         setDataView1(dataView2);
@@ -66,11 +72,11 @@ export default function Dashboard(props) {
 
   // Choose the data view to return/display based on the given option
   const switchDataView = (optionValue) => {
-    if(optionValue === "general") {
+    if (optionValue === "general") {
       return <DataView data={state.data} />;
-    } else if(optionValue === "battery") {
+    } else if (optionValue === "battery") {
       return <BatteryCells data={state.data} />;
-    } else if(optionValue === "minimap") {
+    } else if (optionValue === "minimap") {
       return <MiniMap />;
     } else {
       return <VStack />;
@@ -84,55 +90,55 @@ export default function Dashboard(props) {
   const [graph3, setGraph3] = React.useState("");
 
   // Update the value indicating which graph to display when an option is selected
-  const selectGraph = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(event.target.id === "graphSelect1") {
+  const selectGraph = (event) => {
+    if (event.target.id === "graphSelect1") {
       // Avoid duplicate graphs, unless they are both empty
-      if(event.target.value !== "") {
+      if (event.target.value !== "") {
         // If trying to switch to a graph that is already being displayed in another
         // section, switch the graphs in this section and the other one
-        switch(event.target.value) {
-          case(document.getElementById("graphSelect2").value):
+        switch (event.target.value) {
+          case document.getElementById("graphSelect2").value:
             setGraph2(graph1);
             break;
-          case(document.getElementById("graphSelect3").value):
+          case document.getElementById("graphSelect3").value:
             setGraph3(graph1);
             break;
-          default:
-            break;
+          // default:
+          //   break;
         }
       }
       setGraph1(event.target.value);
-    } else if(event.target.id === "graphSelect2") {
+    } else if (event.target.id === "graphSelect2") {
       // Avoid duplicate graphs, unless they are both empty
-      if(event.target.value !== "") {
+      if (event.target.value !== "") {
         // If trying to switch to a graph that is already being displayed in another
         // section, switch the graphs in this section and the other one
-        switch(event.target.value) {
-          case(document.getElementById("graphSelect1").value):
+        switch (event.target.value) {
+          case document.getElementById("graphSelect1").value:
             setGraph1(graph2);
             break;
-          case(document.getElementById("graphSelect3").value):
+          case document.getElementById("graphSelect3").value:
             setGraph3(graph2);
             break;
-          default:
-            break;
+          // default:
+          //   break;
         }
       }
       setGraph2(event.target.value);
-    } else if(event.target.id === "graphSelect3") {
+    } else if (event.target.id === "graphSelect3") {
       // Avoid duplicate graphs, unless they are both empty
-      if(event.target.value !== "") {
+      if (event.target.value !== "") {
         // If trying to switch to a graph that is already being displayed in another
         // section, switch the graphs in this section and the other one
-        switch(event.target.value) {
-          case(document.getElementById("graphSelect1").value):
+        switch (event.target.value) {
+          case document.getElementById("graphSelect1").value:
             setGraph1(graph3);
             break;
-          case(document.getElementById("graphSelect2").value):
+          case document.getElementById("graphSelect2").value:
             setGraph2(graph3);
             break;
-          default:
-            break;
+          // default:
+          //   break;
         }
       }
       setGraph3(event.target.value);
@@ -141,11 +147,11 @@ export default function Dashboard(props) {
 
   // Choose the graph to return/display based on the given option
   const switchGraph = (optionValue) => {
-    if(optionValue === "battery") {
+    if (optionValue === "battery") {
       return <BatteryGraph />;
-    } else if(optionValue === "power") {
+    } else if (optionValue === "power") {
       return <PowerGraph />;
-    } else if(optionValue === "temperature") {
+    } else if (optionValue === "temperature") {
       return <TemperatureGraph />;
     } else {
       return <VStack />;
@@ -153,32 +159,19 @@ export default function Dashboard(props) {
   };
 
   return (
-    <Grid
-      templateColumns="1fr 2fr"
-      h="100vh"
-      w="100vw"
-    >
-      <GridItem
-        colStart={1}
-        colSpan={1}
-      >
-        <Grid
-          h="100vh"
-          templateRows="1fr 3fr"
-        >
+    <Grid templateColumns="1fr 2fr" h="100vh" w="100vw">
+      <GridItem colStart={1} colSpan={1}>
+        <Grid h="100vh" templateRows="1fr 3fr">
           <GridItem
-              rowStart={1}
-              rowSpan={1}
-              borderColor="black"
-              borderWidth={1}
-              p={2}
+            rowStart={1}
+            rowSpan={1}
+            borderColor="black"
+            borderWidth={1}
+            p={2}
           >
             <FaultsView data={state.data} />
           </GridItem>
-          <GridItem
-              rowStart={2}
-              rowSpan={1}
-          >
+          <GridItem rowStart={2} rowSpan={1}>
             <VStack
               align="stretch"
               spacing={0}
@@ -186,110 +179,124 @@ export default function Dashboard(props) {
               borderWidth={1}
             >
               <Select
-                  id="dataViewSelect1"
-                  size="xs"
-                  variant="filled"
-                  bgColor="grey.300"
-                  placeholder="Select option"
-                  value={dataView1}
-                  onChange={selectDataView}
+                id="dataViewSelect1"
+                size="xs"
+                variant="filled"
+                bgColor="grey.300"
+                placeholder="Select option"
+                value={dataView1}
+                onChange={selectDataView}
               >
                 <DataViewOptions />
               </Select>
-              {(switchDataView)(dataView1)}
+              {switchDataView(dataView1)}
             </VStack>
             <VStack
-                align="stretch"
-                spacing={0}
-                borderColor="black"
-                borderWidth={1}
+              align="stretch"
+              spacing={0}
+              borderColor="black"
+              borderWidth={1}
             >
               <Select
-                  id="dataViewSelect2"
-                  size="xs"
-                  variant="filled"
-                  bgColor="grey.300"
-                  placeholder="Select option"
-                  value={dataView2}
-                  onChange={selectDataView}
+                id="dataViewSelect2"
+                size="xs"
+                variant="filled"
+                bgColor="grey.300"
+                placeholder="Select option"
+                value={dataView2}
+                onChange={selectDataView}
               >
                 <DataViewOptions />
               </Select>
-              {(switchDataView)(dataView2)}
+              {switchDataView(dataView2)}
             </VStack>
           </GridItem>
         </Grid>
       </GridItem>
-      <GridItem
-          colStart={2}
-          colSpan={1}
-      >
-        <Grid
-            h="100vh"
-            templateRows="repeat(3, 1fr)"
-        >
-            <VStack
-              h="100%"
-              align="stretch"
-              spacing={0}
-              borderColor="black"
-              borderWidth={1}
+      <GridItem colStart={2} colSpan={1}>
+        <Grid h="100vh" templateRows="repeat(3, 1fr)">
+          <VStack
+            h="100%"
+            align="stretch"
+            spacing={0}
+            borderColor="black"
+            borderWidth={1}
+          >
+            <Select
+              id="graphSelect1"
+              size="xs"
+              variant="filled"
+              bgColor="grey.300"
+              placeholder="Select option"
+              value={graph1}
+              onChange={selectGraph}
             >
-              <Select
-                  id="graphSelect1"
-                  size="xs"
-                  variant="filled"
-                  bgColor="grey.300"
-                  placeholder="Select option"
-                  value={graph1}
-                  onChange={selectGraph}
-              >
-                <GraphOptions />
-              </Select>
-              {(switchGraph)(graph1)}
-            </VStack>
-            <VStack
-                h="100%"
-                align="stretch"
-                spacing={0}
-                borderColor="black"
-                borderWidth={1}
+              <GraphOptions />
+            </Select>
+            {switchGraph(graph1)}
+          </VStack>
+          <VStack
+            h="100%"
+            align="stretch"
+            spacing={0}
+            borderColor="black"
+            borderWidth={1}
+          >
+            <Select
+              id="graphSelect2"
+              size="xs"
+              variant="filled"
+              bgColor="grey.300"
+              placeholder="Select option"
+              value={graph2}
+              onChange={selectGraph}
             >
-              <Select
-                  id="graphSelect2"
-                  size="xs"
-                  variant="filled"
-                  bgColor="grey.300"
-                  placeholder="Select option"
-                  value={graph2}
-                  onChange={selectGraph}
-              >
-                <GraphOptions />
-              </Select>
-              {(switchGraph)(graph2)}
-            </VStack>
-            <VStack
-                h="100%"
-                align="stretch"
-                spacing={0}
-                borderColor="black"
-                borderWidth={1}
+              <GraphOptions />
+            </Select>
+            {switchGraph(graph2)}
+          </VStack>
+          <VStack
+            h="100%"
+            align="stretch"
+            spacing={0}
+            borderColor="black"
+            borderWidth={1}
+          >
+            <Select
+              id="graphSelect3"
+              size="xs"
+              variant="filled"
+              bgColor="grey.300"
+              placeholder="Select option"
+              value={graph3}
+              onChange={selectGraph}
             >
-              <Select
-                  id="graphSelect3"
-                  size="xs"
-                  variant="filled"
-                  bgColor="grey.300"
-                  placeholder="Select option"
-                  value={graph3}
-                  onChange={selectGraph}
-              >
-                <GraphOptions />
-              </Select>
-              {(switchGraph)(graph3)}
-            </VStack>
+              <GraphOptions />
+            </Select>
+            {switchGraph(graph3)}
+          </VStack>
         </Grid>
       </GridItem>
     </Grid>
+  );
+}
+
+function DataViewOptions(props) {
+  return (
+    <>
+      <option value="general">General Data</option>
+      <option value="battery">Battery Cells</option>
+      <option value="minimap">Minimap</option>
+    </>
+  );
+}
+
+function GraphOptions(props) {
+  return (
+    <>
+      <option value="battery">Battery</option>
+      <option value="power">Power</option>
+      <option value="temperature">Temperature</option>
+    </>
   );
 }
