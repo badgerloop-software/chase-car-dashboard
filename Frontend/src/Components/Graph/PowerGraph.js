d06da9e213fac252de8bfca2022fb8652251faf9
James <jrvollmer@wisc.edu>
2021-12-12 15:03:23 -0600

    Added line charts with fake data

--- Frontend/src/Components/Graph/PowerGraph.js
+++ Frontend/src/Components/Graph/PowerGraph.js
@@ -1,18 +1,86 @@
 import { Center, HStack, Text } from "@chakra-ui/react";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from "react-chartjs-2";
+import faker from 'faker';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 export default function PowerGraph(props) {
-  return (
-    <HStack h="100%" align="stretch">
-      <Text
-        css={{ writingMode: "vertical-lr" }}
-        transform="rotate(180deg)"
-        borderLeftColor="grey.300"
-        borderLeftWidth={1}
-        textAlign="center"
-      >
-        Power
-      </Text>
-      <Center flex={1}>Power Graph</Center>
-    </HStack>
-  );
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: {
+            duration: 0,
+        },
+        plugins: {
+            legend: {
+                position: 'left',
+            },
+            title: {
+                display: true,
+                text: 'Power Chart',
+            },
+        },
+    };
+
+    const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Net Power',
+                // NOTE Remove faker from package.json when actual data is put in
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Motor Power',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+            {
+                label: 'Solar Power',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                borderColor: 'rgb(255, 206, 86)',
+                backgroundColor: 'rgba(255, 206, 86, 0.5)',
+            },
+        ],
+    };
+
+    return (
+        <HStack h="100%" align="stretch" >
+            <Text
+                css={{ writingMode: "vertical-lr" }}
+                transform="rotate(180deg)"
+                borderLeftColor="grey.300"
+                borderLeftWidth={1}
+                textAlign="center"
+            >
+                Power
+            </Text>
+            <Center flex={1}>
+                <Line options={ options } data={ data } />
+            </Center>
+        </HStack>
+    );
 }
