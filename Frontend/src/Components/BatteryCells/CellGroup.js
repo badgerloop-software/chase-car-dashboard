553186eab03ac4661d6eaf8592dae4c47e491b53
James <jrvollmer@wisc.edu>
2021-12-15 01:02:25 -0600

    Updated data

--- Frontend/src/Components/BatteryCells/CellGroup.js
+++ Frontend/src/Components/BatteryCells/CellGroup.js
@@ -3,20 +3,19 @@ import { Text, VStack } from "@chakra-ui/react";
 export default function CellGroup(props) {
     return (
         <VStack
-            flexWrap="wrap"
             borderWidth={1}
             borderColor="black"
             spacing={0}
             padding={1}
         >
             <Text as='u' fontSize='sm'>
-                Cell Group {props.groupNum}
+                Group {props.groupNum}
             </Text>
-            <Text fontSize='sm'>
-                V: {props.voltage}
+            <Text fontSize='xs'>
+                V: {props.voltage}V
             </Text>
-            <Text fontSize='sm'>
-                I: {props.current}
+            <Text fontSize='xs'>
+                T: {props.temperature}&deg;C
             </Text>
         </VStack>
     );
