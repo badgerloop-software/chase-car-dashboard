1ee0e0c6b88896dc2729b06c5faac84c666b6bd0
James <jrvollmer@wisc.edu>
2021-12-16 00:52:51 -0600

    Updated labels

--- Frontend/src/Components/Graph/TemperatureGraph.js
+++ Frontend/src/Components/Graph/TemperatureGraph.js
@@ -46,7 +46,7 @@ export default function TemperatureGraph(props) {
         },
     };
 
-    const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const labels = props.data?.timestamps ?? [10,9,8,7,6,5,4,3,2,1];
 
     const data = {
         labels,
