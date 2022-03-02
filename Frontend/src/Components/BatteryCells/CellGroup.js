80fafe15cdea64b9ebe369c455025d5740294efb
jrvollmer <72111610+jrvollmer@users.noreply.github.com>
2022-03-01 18:48:37 -0600

    Removed whitespace and standard quotes

--- Frontend/src/Components/BatteryCells/CellGroup.js
+++ Frontend/src/Components/BatteryCells/CellGroup.js
@@ -4,7 +4,7 @@ export default function CellGroup(props) {
     return (
         <VStack
             borderWidth={1}
-            borderColor="black"
+            borderColor='black'
             spacing={0}
             padding={1}
         >
@@ -14,12 +14,9 @@ export default function CellGroup(props) {
             <Text fontSize='sm'>
                 {props.voltage.toFixed(2)}V
             </Text>
-
-            <Box border="2px" borderRadius='md' w = '3.5vw' h = '2.5vh'>
+            <Box border='2px' borderRadius='md' w='3.5vw' h='2.5vh'>
                 {props.battery}
             </Box>
-
-
         </VStack>
     );
 }
