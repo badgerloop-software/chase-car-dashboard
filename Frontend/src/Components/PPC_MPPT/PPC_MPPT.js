48ba48a1585d167f7c67332b006b95754800cf87
Charles Huai <huai2@wisc.edu>
2022-03-29 19:11:13 -0500

    SW-123 Temp layout  of PPC_MPPT

--- Frontend/src/Components/PPC_MPPT/PPC_MPPT.js
+++ Frontend/src/Components/PPC_MPPT/PPC_MPPT.js
@@ -2,15 +2,15 @@ import { Box, Flex, Spacer, Grid, GridItem, Heading, Text} from "@chakra-ui/reac
 
 export default function PPC_MPPT(props) {
     return (
-        <Flex direction = 'row'> 
+        <Flex h='100%' direction = 'row'> 
             <Flex w='50%'> hi </Flex>
             <Grid
-                templateRows='repeat(8, 1fr)'
+                templateRows='repeat(9, 1fr)'
                 templateColumns='repeat(2, 1fr)'
                 w='50%'
             >
                 <GridItem colSpan={2} rowSpan={1}>
-                    <Box w='100%' bg='#DDDDDD' borderWidth='1px' borderColor='black'>
+                    <Box w='100%' h='100%' bg='#DDDDDD' borderWidth='1px' borderColor='black'>
                             <Heading size = 'xs' textAlign = 'center'>Power Path Controller</Heading>
                     </Box>
                 </GridItem>
@@ -23,11 +23,27 @@ export default function PPC_MPPT(props) {
                 </GridItem>
 
                 <GridItem colSpan={2} rowStart={4}>
-                    <Box w='100%' bg='#DDDDDD' borderWidth='1px' borderColor='black'>
+                    <Box w='100%' h='100%' bg='#DDDDDD' borderWidth='1px' borderColor='black'>
                             <Heading size = 'xs' textAlign = 'center'>Maximum Power Point Tracker</Heading>
                     </Box>
                 </GridItem>
-                <GridItem></GridItem>
+
+                <GridItem colStart={1} rowStart={5} rowSpan={2}> 
+                    <Box w='100%' h='100%' borderWidth='1px' borderColor='black'>
+                        Output current
+                    </Box>
+                </GridItem>
+                <GridItem colStart={2} rowStart={5} rowSpan={2}>
+                    <Box w='100%' h='100%' borderWidth='1px' borderColor='black'>
+                        Charge + MPPT
+                    </Box>
+                </GridItem>
+
+                <GridItem colSpan={2} rowStart={7} rowSpan={3}>
+                    <Box w='100%' h='100%' bg='Tomato' borderWidth='1px' borderColor='black'>
+
+                    </Box>
+                </GridItem>
             </Grid>
             {/* <Flex h='100%'w='50%' direction = 'column' borderWidth='1px' borderColor='black'>  
                 <Flex h='30%' direction='column'>
