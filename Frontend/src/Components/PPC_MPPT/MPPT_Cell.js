5704773c7a6a67be0ce6490b5543ab8be5ac95d1
Charles Huai <huai2@wisc.edu>
2022-04-07 12:39:35 -0500

    SW-123 resized mppt_cells and removed redundant attributes

--- Frontend/src/Components/PPC_MPPT/MPPT_Cell.js
+++ Frontend/src/Components/PPC_MPPT/MPPT_Cell.js
@@ -6,10 +6,9 @@ export default function MPPT_Cell(props){
             h='45%' 
             bg={props.boolean? '#05FF00':'#FF010140'}
             border='2px' 
-            textAlign='center'
             lineHeight='1.6em'
         >
-            <Text as='b' fontSize='1.5vw'>{props.label}</Text>
+            <Text as='b' fontSize='1.3vw'>{props.label}</Text>
         </Center>
     )
 
