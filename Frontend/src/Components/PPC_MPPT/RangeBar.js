60c8c4139b3f947d8da602db17a38301fa65d434
James <jrvollmer@wisc.edu>
2022-04-12 01:12:54 -0500

    Made functional component for consistency

--- Frontend/src/Components/PPC_MPPT/RangeBar.js
+++ Frontend/src/Components/PPC_MPPT/RangeBar.js
@@ -1,25 +1,18 @@
-import React from "react";
 import { Box } from "@chakra-ui/react";
 
+export default function RangeBar(props) {
+  const clamp = (x, a, b) => Math.max(a, Math.min(x, b));
 
-export default class RangeBar extends React.Component {
-  clamp = (x, a, b) => Math.max(a, Math.min(x, b));
+  const toPercentage = (temp, minv, maxv) => 100 * clamp(temp / (maxv - minv), 0, 1);
 
-  toPercentage = (temp, minv, maxv) => 100 * this.clamp(temp / (maxv - minv), 0, 1);
+  let valp = toPercentage(props.val, props.min, props.max)
 
-  render() {
-    let valp = this.toPercentage(this.props.val, this.props.min, this.props.max)
-    return (
-        <>
-            <Box
-                border="2px"
-                w={this.props.w ?? "15vw"}
-                h={this.props.h ?? "1.25vh"}
-                css={{
-                    background: "linear-gradient(to right, #52FF00 " + valp + "%, #FFFFFF " + valp + "% 100%)"
-                }}
-             />
-        </>
-      );
-  }
+  return (
+    <Box
+      border="2px"
+      w={props.w ?? "15vw"}
+      h={props.h ?? "1.25vh"}
+      css={{ background: "linear-gradient(to right, #52FF00 " + valp + "%, #FFFFFF " + valp + "% 100%)" }}
+      />
+  );
 }
