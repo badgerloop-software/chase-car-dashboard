da0d4a82d85f3bdeb1dd803c94295abc1f9e0414
James <jrvollmer@wisc.edu>
2022-04-13 00:46:47 -0500

    Changed component/file names to match

--- Frontend/src/Components/GeneralData/CommsLabel.js
+++ Frontend/src/Components/GeneralData/CommsLabel.js
@@ -2,7 +2,7 @@ import {Flex, Image, Text} from "@chakra-ui/react";
 import CheckMark from "./DriverIcons/Check Mark.png"
 import CrossMark from "./DriverIcons/Cross Mark.png"
 
-export default function CommsCell(props) {
+export default function CommsLabel(props) {
     return(
         <Flex alignItems='center'>
             {props.boolean ? <Image boxSize='2vh' src={CheckMark}/> : <Image boxSize='2vh' src={CrossMark}/>}
