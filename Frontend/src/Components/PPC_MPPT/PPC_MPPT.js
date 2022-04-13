5d421082709665925fc1ec9fbb06d3ef2df0460d
James <jrvollmer@wisc.edu>
2022-04-12 20:26:33 -0500

    Removed PPC_MPPT/Heading_Cell.js

--- Frontend/src/Components/PPC_MPPT/PPC_MPPT.js
+++ Frontend/src/Components/PPC_MPPT/PPC_MPPT.js
@@ -2,13 +2,13 @@ import {Flex, Spacer, VStack} from "@chakra-ui/react";
 import PPC_Cell from "./PPC_Cell";
 import MPPT_Cell from "./MPPT_Cell";
 import OutCurr_Cell from "./OutCurr_Cell";
-import Heading_Cell from "./Heading_Cell";
+import Heading_Cell from "../GeneralData/Heading_Cell";
 import Range_Cell from "../GeneralData/Range_Cell";
 
 
 export default function PPC_MPPT(props) {
     return (
-        <Flex h='100%'> 
+        <Flex h='100%'>
             <VStack
                 flex='1'
                 align='stretch'
@@ -21,7 +21,7 @@ export default function PPC_MPPT(props) {
                 spacing={0}
                 borderLeft='1px'
             >
-                <Heading_Cell label='Power Path Controller'/>
+                <Heading_Cell fontSize='1.1vw' label='Power Path Controller'/>
                 <Flex flex='2' p='1'>
                       <PPC_Cell
                           boolean={props.data?.supplemental_valid[0]}
@@ -33,7 +33,7 @@ export default function PPC_MPPT(props) {
                           label='Main Battery Pack'
                       />
                 </Flex>
-                <Heading_Cell label='Maximum Power Point Tracker'/>
+                <Heading_Cell fontSize='1.1vw' label='Maximum Power Point Tracker'/>
                 <Flex flex = '8' direction='column' pb='1'>
                     <Flex pl='2' pr='2' pt='1'>
                         <OutCurr_Cell
