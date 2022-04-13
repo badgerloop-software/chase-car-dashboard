c2d2165221da9fa813e46b933f934dce360cb502
millewave <40739837+millewave@users.noreply.github.com>
2022-04-12 21:06:17 -0500

    SW-121 renamed Comms_Cell to CommsCell
    Co-authored-by: jrvollmer <72111610+jrvollmer@users.noreply.github.com>

--- Frontend/src/Components/GeneralData/Comms_Cell.js
+++ Frontend/src/Components/GeneralData/Comms_Cell.js
@@ -2,7 +2,7 @@ import {Flex, Image, Text} from "@chakra-ui/react";
 import CheckMark from "./DriverIcons/Check Mark.png"
 import CrossMark from "./DriverIcons/Cross Mark.png"
 
-export default function Comms_Cell(props) {
+export default function CommsCell(props) {
     return(
         <Flex alignItems='center'>
             {props.boolean ? <Image boxSize='2vh' src={Check_Mark}/> : <Image boxSize='2vh' src={Cross_Mark}/>}
