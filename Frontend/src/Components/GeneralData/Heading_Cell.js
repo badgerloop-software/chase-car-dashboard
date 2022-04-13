e171d88005b0a22bbbacab671c8f59b50ea7cc15
millewave <40739837+millewave@users.noreply.github.com>
2022-04-12 21:09:58 -0500

    SW-121 renamed Heading_Cell to HeadingCell
    Co-authored-by: jrvollmer <72111610+jrvollmer@users.noreply.github.com>

--- Frontend/src/Components/GeneralData/Heading_Cell.js
+++ Frontend/src/Components/GeneralData/Heading_Cell.js
@@ -1,6 +1,6 @@
 import {Box, Heading} from "@chakra-ui/react";
 
-export default function Heading_Cell(props) {
+export default function HeadingCell(props) {
     return (
         <Box bg='#DDDDDD' borderTop='1px' borderBottom='1px' textAlign='center' lineHeight='1'>
             <Heading as='b' fontSize={props.fontSize}>{props.label}</Heading>
