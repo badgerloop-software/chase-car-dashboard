0df083fe852a0fd9cdb44e63a3e22bb62cb0b5f3
James <jrvollmer@wisc.edu>
2022-04-14 01:04:59 -0500

    Added data format constants and script to auto-populate it

---
+++ Frontend/src/CustomScripts/updateDataConstants.mjs
@@ -0,0 +1,22 @@
+import { writeFileSync } from "fs";
+import DATA_FORMAT from "../../../Backend/Data/sc1-data-format/format.json";
+
+const constantsJson = {};
+
+// Loop through data labels in data format
+for(const property in DATA_FORMAT) {
+    // If the current property is timestamp data, skip it
+    if (property.startsWith("tstamp")) continue;
+
+    // Get list of info for current property
+    const [, , unit, min, max,] = DATA_FORMAT[property];
+
+    // Add current property and its list of info/constants to JSON
+    constantsJson[property] = {
+        "UNIT": unit,
+        "MIN": min,
+        "MAX": max
+    };
+}
+
+writeFileSync("./src/data-constants.json", JSON.stringify(constantsJson, null, 4));
