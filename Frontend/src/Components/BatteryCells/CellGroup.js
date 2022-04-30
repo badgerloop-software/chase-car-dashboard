6c0077612d6a8d4506c190bdd615fb68c1c16c0f
kappa54 <z3xcvv@gmail.com>
2022-04-30 01:39:28 -0500

    nominal warnings; incorporate data-constants

--- Frontend/src/Components/BatteryCells/CellGroup.js
+++ Frontend/src/Components/BatteryCells/CellGroup.js
@@ -1,18 +1,24 @@
 import { Box, Text, VStack } from "@chakra-ui/react";
+import CONSTANTS from "../../data-constants.json";
 
 export default function CellGroup(props) {
+    const voltageConstants = CONSTANTS[`cell_group${props.groupNum}_voltage`];
+    const bg = (props.voltage < voltageConstants.MIN || props.voltage > voltageConstants.MAX) ?
+        "#ff000055" : null;
+
     return (
         <VStack
             borderWidth={1}
             borderColor='black'
             spacing={0}
             padding={1}
+            backgroundColor={bg}
         >
             <Text as='u' fontSize='sm'>
                 Group {props.groupNum}
             </Text>
             <Text fontSize='sm'>
-                {props.voltage.toFixed(2)} V
+                {props.voltage.toFixed(2)} {voltageConstants.UNIT}
             </Text>
             <Box border='2px' borderRadius='md' w='90%' h='1.5vh'>
                 {props.battery}
