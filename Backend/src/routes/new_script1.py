7a730ea8b29f2e085a0025c048e6ec06579780bf
James <jrvollmer@wisc.edu>
2022-08-10 18:05:52 -0500

    Recorded data conversion script is working

--- Backend/src/routes/new_script1.py
+++ Backend/src/routes/new_script1.py
@@ -3,7 +3,7 @@ import time
 import json
 import csv
 import numpy as np
-import os # TODO Just for getting the size of the binary file for debugging
+import os # TODO Disregard: Just for getting the size of the binary file for debugging
 
 print('#Hello from python#')
 print('First param:'+sys.argv[1]+'#')
@@ -13,29 +13,27 @@ print('Second param:'+sys.argv[2])
 #f = open(sys.argv[1], mode='rb')
 # TODO "./Data/demofile2.txt", "a" worked when spawned in backend
 
+recordedDataPath = sys.argv[1]
+
 f2 = open("./demofile2.txt", "a")
 f3 = open(sys.argv[2])
 
-f4 = open("./demo2.bin", "rb") # TODO Check the file name
+f4 = open(recordedDataPath, "rb") # TODO Check the file name
 
 # TODO f5 = open("src/routes/test.csv", "w")
 f5 = open("./test.csv", "w", newline='')
 f5writer = csv.writer(f5, dialect='excel', delimiter=',')
 
+# Load data format JSON and get the keys in it
 format = json.load(f3)
-
-#print(format.keys())
+keys = format.keys()
 
 f2.write('d\n')
 #f2.write(str(format))
 
-# TODO print(format.keys())
-print("\nKeys were here\n") # TODO
-#keys = format.keys()
-
 j=0
 i=0
-for key in format.keys():
+for key in keys:
 	i += format[key][0]
 	if(j <= 3):
 		f2.write(key + '\n')
@@ -43,22 +41,32 @@ for key in format.keys():
 		# TODO time.sleep(1)
 
 print("Bytes in data packet: " + str(i))
-print("Bytes in binary file: " + str(os.path.getsize("./demo2.bin")))
-print("Number of rows that should be loaded: " + str(os.path.getsize("./demo2.bin") / i))
+print("Bytes in binary file: " + str(os.path.getsize(recordedDataPath)))
+print("Number of rows that should be loaded: " + str(os.path.getsize(recordedDataPath) / i))
 
-# Write headers to the file TODO Will have to account for and remove the tstamp elements. Will also have to add solar_car_connection
-f5writer.writerow(list(format.keys()) + ["timestamp"])
 
-# TODO
+# Remove timestamp components from headers. If any of them cannot be found in the data format, print a descriptive message so the user knows what happened
+headers = list(keys)
+for toRemove in ["tstamp_ms", "tstamp_sc", "tstamp_mn", "tstamp_hr"]:
+	if(not toRemove in headers):
+		print(f"'{toRemove}' was not found in '{sys.argv[2]}'")
+	headers.remove(toRemove)
 
-print(np.fromfile(f4, np.dtype('<u2'), 1, "", 51))
+# Write headers to the file
+# TODO Will also have to add solar_car_connection (to this and data recording)
+f5writer.writerow(headers + ["timestamp"])
 
-data = 1 # Initialize data
-#fun unpackData(data):
-while data: # Loop until there is no more data left in the file
+
+# TODO Showed the tstamp_ms value: print(np.fromfile(f4, np.dtype('>u2'), 1, "", 51))
+
+
+# Get the size of the recorded data file
+recordedDataSize = os.path.getsize(recordedDataPath)
+while (f4.tell() < recordedDataSize):# TODO data: # Loop until there is no more data left in the file
+	data = 0 # Data to add to the current row
 	offset = 0 # Byte offset for the buffer array
 	timestamp = "::."# TODO s = solarCarData["timestamps"] # The array of timestamps for each set of data added to solarCarData
-	row = []
+	row = [] # Row to be added to the file
 	# Array values indicate the status of the connection to the solar car. These will always be true when unpacking data
 	# TODO This will have to be accounted for eventually: solar_car_connection = solarCarData["solar_car_connection"]
 	
@@ -76,7 +84,7 @@ while data: # Loop until there is no more data left in the file
 	#if (solar_car_connection.length > X_AXIS_CAP) solar_car_connection.pop()
 	#solarCarData["solar_car_connection"] = solar_car_connection
 	
-	for property in format.keys():
+	for property in keys:
 		# TODO dataArray = [] # Holds the array of data specified by property that will be put in solarCarData
 		#dataType = "" # Data type specified in the data format
 		
@@ -88,16 +96,18 @@ while data: # Loop until there is no more data left in the file
 		# Get the next piece of data from the file
 		if (dataType == "uint8"):
 			# TODO dataType = np.dtype('u1')
-			data = np.fromfile(f4, np.dtype('u1'), 1, "", offset)
+			data = np.fromfile(f4, np.dtype('u1'), 1, "", 0) #offset)
 			
 			# Add hours, minutes, or seconds to the timestamp if the current piece of data is tstamp_hr/mn/sc
 			if (property == "tstamp_hr"):
+				data = data[0]
 				if (data < 10):
 					timestamp = "0" + str(data) + timestamp
 				else:
 					timestamp = str(data) + timestamp
 				#break
 			elif (property == "tstamp_mn"):
+				data = data[0]
 				# TODO Update this: Ternary operator (and probably replace function) don't work the same in Python
 				timestamp = timestamp.replace(
  					"::",
@@ -105,6 +115,7 @@ while data: # Loop until there is no more data left in the file
 				)
 				#break
 			elif (property == "tstamp_sc"):
+				data = data[0]
 				timestamp = timestamp.replace(
 					":.",
 					":" + (("0" + str(data)) if (data < 10) else str(data)) + "."
@@ -114,11 +125,11 @@ while data: # Loop until there is no more data left in the file
 			#break
 		elif (dataType == "float"):
 			# TODO dataType = np.dtype('<f4') #TODO Remove the 4?
-			data = np.fromfile(f4, np.dtype('<f4'), 1, "", offset) # TODO Remove the 4 in the data type?
+			data = np.fromfile(f4, np.dtype('<f4'), 1, "", 0) #offset) # TODO Remove the 4 in the data type?
 			#break
 		elif (dataType == "bool"):
 			# TODO dataType = np.dtype('?')
-			data = np.fromfile(f4, np.dtype('?'), 1, "", offset)
+			data = np.fromfile(f4, np.dtype('?'), 1, "", 0) #offset)
 			#break
 		elif (dataType == "char"):
 			#dataType = np.dtype('U1') # TODO
@@ -129,12 +140,12 @@ while data: # Loop until there is no more data left in the file
 			#data = np.fromfile(f4, np.dtype('u1'), 1, "", offset) # TODO Remove; just for testing
 			
 			# TODO Should be able to use i1 instead of u1 since the random number generator in DataGenerator and the actual values will never go above 100 (and will not have a msb of 1)
-			data = np.fromfile(f4, np.dtype('u1'), 1, "", offset) # TODO Check data type
+			data = np.fromfile(f4, np.dtype('u1'), 1, "", 0) #offset) # TODO Check data type
 			data = chr(int(data))
 			#break
 		elif (dataType == "uint16"):
 			# TODO dataType = np.dtype('u2')
-			data = np.fromfile(f4, np.dtype('>u2'), 1, "", offset)
+			data = np.fromfile(f4, np.dtype('>u2'), 1, "", 0)[0] #offset)
 			
 			# Add milliseconds to the timestamp if the current piece of data is tstamp_ms
 			if (property == "tstamp_ms"):
@@ -154,7 +165,7 @@ while data: # Loop until there is no more data left in the file
 			#break
 		else:
 			# If the data type does not match any of the previous values, default to a string with n characters, where n is the byte count listed in the data format
-			data = np.fromfile(f4, np.dtype('U'+format[property][0]), 1, "", offset)
+			data = np.fromfile(f4, np.dtype('U'+format[property][0]), 1, "", 0) #offset)
 			print("WARNING: default case reached when unpacking recorded data") # TODO Elaborate if there are multiple unpacking scripts
 			#break
 		
@@ -314,11 +325,13 @@ while data: # Loop until there is no more data left in the file
 		
 		# Add the piece of data to the row, provided it is not part of the timestamp
 		# TODO Use a more graceful/compact condition
-		if (property != "tstamp_sc" and property != "tstamp_mn" and property != "tstamp_hr"):
-			print("\n----------\n" + property + ": " + str(data)) # TODO
-			print("----------\nFirst element of " + property + ": " + str(data[0])) # TODO
+		if (not property.startswith("tstamp")):# TODO (property != "tstamp_ms" and property != "tstamp_sc" and property != "tstamp_mn" and property != "tstamp_hr"):
+			#print("\n----------\n" + "Offset: " + str(offset)) # TODO
+			#print("----------\n" + "File location: " + str(f4.tell())) # TODO
+			#print("----------\n" + property + ": " + str(data)) # TODO
+			#print("----------\nFirst element of " + property + ": " + str(data[0])) # TODO
 			row += [data[0]] # np.fromfile() returns an array, and the first element is the actual value, so add the first element to the row
-			print(row) # TODO
+			#print(row) # TODO
 		
 		# Increment offset by amount specified in data format
 		offset += format[property][0]
@@ -327,7 +340,7 @@ while data: # Loop until there is no more data left in the file
 	row += [timestamp]
 	
 	#TODO
-	print(row)
+	# TODO print(row)
 	# Write the row to the csv file
 	f5writer.writerow(row)
 	
