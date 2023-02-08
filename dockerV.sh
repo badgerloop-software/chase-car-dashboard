951738b75ab706fe1566597f67384eb369f13fca
vikrsnth <vikmandadi@gmail.com>
2023-02-08 14:26:57 -0600

    update to the bash script

--- dockerV.sh
+++ dockerV.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker pull ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
+open http://localhost:4000
 docker run -p 4000:3000 ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
-open https://localhost:3000
+
