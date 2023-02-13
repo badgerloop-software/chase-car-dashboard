53d7abfeef3812b7d3d4755ff3e3f0b32e82fc2f
Sarthak Agrawal <68310924+sarthyparty@users.noreply.github.com>
2023-02-12 19:12:09 -0600

    added instructions to run application using docker to readme

--- windows-pull-run.bat
+++ windows-pull-run.bat
@@ -1,7 +1,16 @@
 @echo off
-docker pull ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
-start http://localhost:4000
-docker run -p 4000:3000 ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
 
-@REM #The server will be run at http://localhost:4000, it will take one to two minutes to start up
+if not "%1"=="" (
+    if not "%1"=="-o" (
+        echo script usage: windows-pull-run.bat [-o]
+        echo     -o: disables automatic opening of the browser
+        exit /b 1
+    )
+)
+docker pull ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
+if not "%1"=="-o" start http://localhost:4000
+
+docker run -p 3000:3000 ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
+
+@REM #The server will be run at http://localhost:3000, it will take one to two minutes to start up
 @REM #if this window does not automatically pop up then please enter the URL manually
