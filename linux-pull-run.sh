2fdbc87db8f276f5dd402157ecffc694b41232ca
vikranth <vikmandadi@gmail.com>
2023-02-17 18:49:20 -0600

    updated scripts with comments for instructions

--- linux-pull-run.sh
+++ linux-pull-run.sh
@@ -4,3 +4,5 @@ docker pull ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
 [[ $OSTYPE == 'darwin'* ]] && open http://localhost:4000
 docker run -p 4000:3000 ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
 
+#The server will be run at http://localhost:4000, it will take one to two minutes to start up
+#if this window does not automatically pop up then please enter the URL manually
--- windows-pull-run.bat
+++ windows-pull-run.bat
@@ -2,3 +2,6 @@
 docker pull ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
 start http://localhost:4000
 docker run -p 4000:3000 ghcr.io/badgerloop-software/chase-car-dashboard-image:latest
+
+@REM #The server will be run at http://localhost:4000, it will take one to two minutes to start up
+@REM #if this window does not automatically pop up then please enter the URL manually
