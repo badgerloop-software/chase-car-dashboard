61dc96bf35bedc9070733d29946fab14926709bd
James <jamesvollmer02@gmail.com>
2023-03-20 20:23:45 -0500

    Data recording backend
    Implemented a method for recording data coming from the solar car to
    the user's filesystem. Data is recorded to a binary file in real time,
    and that raw binary file can be converted to a formatted Excel csv using
    a Python script.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { createServer } from "http";
 import index from "./routes/api.js";
+import cors from 'cors';
 import CONSTANTS from "../src/constants.json";
 
 const PORT = CONSTANTS.PORT;
 const APP = express();
+const bodyParser = require('body-parser');
+APP.use(bodyParser.json());
+APP.use(cors({
+    // Accepted Origin
+    origin: 'http://localhost:3000'
+}));
 APP.use(index);
 
 const SERVER = createServer(APP);
