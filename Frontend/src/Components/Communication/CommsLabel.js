a73991cedb0ddce7a80c2093b6898c6775da7d58
Jack Wherry <jwherry@wisc.edu>
2023-04-03 21:47:49 -0500

    implement communication component
    remove GeneralData and add indent prop

--- Frontend/src/Components/Communication/CommsLabel.js
+++ Frontend/src/Components/Communication/CommsLabel.js
@@ -1,10 +1,11 @@
-import {Flex, Image, Text} from "@chakra-ui/react";
+import {Flex, Box, Image, Text} from "@chakra-ui/react";
 import CheckMark from "./DriverIcons/Check Mark.png"
 import CrossMark from "./DriverIcons/Cross Mark.png"
 
 export default function CommsLabel(props) {
     return(
         <Flex alignItems='center'>
+            {props.indent && <Box w='3vh' />}
             {props.boolean ? <Image boxSize='2vh' src={CheckMark}/> : <Image boxSize='2vh' src={CrossMark}/>}
             <Text fontSize='2vh'>&#160;{props.label}</Text>
         </Flex>
