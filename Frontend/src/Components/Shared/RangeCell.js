8a9e4200dfbb62154491647b6e16c186b931a7b2
Charles Huai <huai2@wisc.edu>
2023-04-15 22:44:50 -0500

    Created Motor/Motion component

--- Frontend/src/Components/Shared/RangeCell.js
+++ Frontend/src/Components/Shared/RangeCell.js
@@ -12,13 +12,14 @@ export default function RangeCell(props) {
             <Flex flex='0.4' w={props.w}>
                 <Text fontSize={props.fontSize}>{props.label}</Text>
                 <Spacer/>
-                <Text fontSize={props.fontSize}>{props.data.toFixed(props.digits)}{props.unit}</Text>
+                <Text fontSize={props.fontSize}>{props.data.toFixed(props.digits)} {props.unit}</Text>
             </Flex>
             <RangeBar
                 val={props.data ?? -1}
                 min={props.min ?? -1}
                 max={props.max ?? 0}
                 w={props.w}
+                h={props.rangeBarH}
             />
         </Flex>
     );
