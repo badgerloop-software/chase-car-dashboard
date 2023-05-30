1cce780bcd8629d7cf0b9fac57670b8aca6e4ba0
Jack Wherry <jwherry@wisc.edu>
2023-05-30 11:45:32 -0500

    generalize color changes from PPC/MPPT

--- Frontend/src/Components/PPC_MPPT/PPC_CELL.js
+++ Frontend/src/Components/PPC_MPPT/PPC_CELL.js
@@ -5,10 +5,10 @@ export default function PPC_CELL(props) {
     const { colorMode } = useColorMode();
 
     const borderCol = getColor("border", colorMode);
-    const greenBgCol = getColor("ppc_mppt_greenBg", colorMode);
-    const greenTxtCol = getColor("ppc_mppt_txtGreenBg", colorMode);
-    const redBgCol = getColor("ppc_mppt_redBg", colorMode);
-    const redTxtCol = getColor("ppc_mppt_txtRedBg", colorMode);
+    const greenBgCol = getColor("greenBg", colorMode);
+    const greenTxtCol = getColor("txtGreenBg", colorMode);
+    const redBgCol = getColor("redBg", colorMode);
+    const redTxtCol = getColor("txtRedBg", colorMode);
 
     return (
         <Center
