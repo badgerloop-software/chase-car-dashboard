335ca87d47c69bbb51e4c9b5d3b42302de95123d
Mingcan Li <mli727@wisc.edu>
2023-07-29 22:11:22 +0800

    add components directory

--- Backend/framework/router.py
+++ Backend/framework/router.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
 from core import core_api
+from components import component_router
 def register_route(app : FastAPI):
     app.include_router(core_api.router, tags=['Core'])
+    app.include_router(component_router.router, prefix='/components', tags=['components'])
