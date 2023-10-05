b477b22db6cbae1daed7bd92f1e67f90ecd313d0
Mingcan Li <mli727@wisc.edu>
2023-10-05 10:28:36 -0500

    Add configuration settings

--- Backend/config.py
+++ Backend/config.py
@@ -1,8 +1,12 @@
 import os
 
 DATAFORMAT_PATH = f'{os.getcwd()}/Data/sc1-data-format/format.json'
-PORT = 4001
+HOST_PORT = 4001
 
 REDIS_URL = '127.0.0.1'
 REDIS_PORT = 6379
 REDIS_DB = 0
+
+CAR_IP = '192.168.1.15'
+LOCAL_IP = 'localhost'
+DATA_PORT = 4003
