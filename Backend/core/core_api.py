dba141e9a396bcb347bbd6b6d583fa7617875df5
Jack Wherry <jwherry@wisc.edu>
2023-11-03 19:25:55 -0500

    make Excel files contain readable dates

--- Backend/core/core_api.py
+++ Backend/core/core_api.py
@@ -31,9 +31,11 @@ async def get_processed_data(start_time, end_time):
 
     result = await db.query_without_aggregation(all_keys, start_time, end_time)
 
+    result.index = pd.to_datetime(result.index, unit='ms')
+
     # see https://stackoverflow.com/a/63989481
     output = io.BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    writer = pd.ExcelWriter(output, engine='xlsxwriter', datetime_format='MM-DD HH:MM:SS')
     result.to_excel(writer, sheet_name='Sheet1')
 
     writer.close()
