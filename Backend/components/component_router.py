f9b14f42a3310a30dfddb349e8c0edb2dfbe58dd
Mingcan Li <mli727@wisc.edu>
2024-01-30 16:21:15 -0600

    move data recording out of core_api.py

--- Backend/components/component_router.py
+++ Backend/components/component_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import graph_api
+from . import graph_api, record_data
 
 router = APIRouter()
 
 router.include_router(graph_api.router)
+router.include_router(record_data.router)
