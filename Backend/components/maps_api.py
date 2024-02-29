f3b1707072701f452360fab018ebd7ca81560c75
Lukas <lawruss17@gmail.com>
2024-02-28 21:32:42 -0600

    google maps wip

---
+++ Backend/components/maps_api.py
@@ -0,0 +1,54 @@
+from core import db
+from fastapi import APIRouter
+import numpy as np
+import config
+
+router = APIRouter()
+
+@router.get("/maps")
+async def get_maps(data: str, start_time: int, end_time: int):
+    """pass in a comma separated list of data that the frontend requests. start_time and end_time must be in unix milliseconds"""
+    if not data:
+        # use a query to get timestamp
+        query_result = await db.query(['speed'], start_time, end_time, ['avg'],
+                                      (end_time - start_time) // 60 or 1)
+        return {'response': {"timestamps": query_result.index.to_list()}}
+    data = [d.strip() for d in data.split(',')]
+    # sort the data into smaller group by aggregation methods
+    group = {'avg': [], 'first':[], 'last':[], 'max': [], 'min':[], 'no-aggr':[], 'sum': []}
+    for d in data:
+        # TODO change this to actual aggregation specified in data format
+        group['no-aggr'].append(d) if config.FORMAT[d]['type']=='bool' else group['avg'].append(d)
+    result = dict()
+    for key in group:
+        if group[key]:
+            #query the database for each aggregation method
+            query_result = {}
+            if key != 'no-aggr':
+                query_result = await db.query(group[key], start_time, end_time, [key]*len(group[key]), (end_time-start_time)//60 or 1)
+            else:
+                query_result = await db.query_without_aggregation(group[key], start_time, end_time)
+            time_stamp = query_result.index.to_list()
+            # add the tuples to the json
+            for name in group[key]:
+                result[name] = []
+                data_arr = query_result[name].to_list()
+                if key == 'no-aggr': # TODO Replace 'no-aggr' with min-max key
+                    prev_y = -100
+                    prev_y_count = 1
+                    for i in range(len(data_arr)):
+                        # only include the start and end time of a period of same data
+                        if data_arr[i] != prev_y:
+                            if prev_y_count > 1:
+                                result[name].append({'x': time_stamp[i-1], 'y': data_arr[i-1]})
+                            result[name].append({'x': time_stamp[i], 'y': data_arr[i]})
+                            prev_y = data_arr[i]
+                            prev_y_count = 1
+                        elif i == len(data_arr)-1:
+                            result[name].append({'x': time_stamp[i], 'y': data_arr[i]})
+                        else:
+                            prev_y_count += 1
+                else:
+                    result[name] = [{'x': time_stamp[i], 'y': data_arr[i]} for i in range(len(data_arr))]
+
+    return {'response': result}
---
+++ Frontend/src/Components/GoogleMap/Map.js
@@ -0,0 +1,56 @@
+import { Map, Marker, Polyline, GoogleApiWrapper } from "google-maps-react";
+
+let coords = [];
+let values = [];
+
+function getCoordsAndValue() {
+  // call api to get new coords and new other value
+
+  coords.push({ lat: 25.774, lng: -80.19 });
+  coords.push({ lat: 18.466, lng: -66.118 });
+  coords.push({ lat: 32.321, lng: -64.757 });
+  coords.push({ lat: 25.774, lng: -80.19 });
+
+  values.push(100);
+
+  // is this return necessary? CarMap() can just get the coords and values since global variables
+  return coords, values;
+}
+
+function calculateColor(value) {
+  // calculate polyline color based on value
+  return "#000000";
+}
+
+function CarMap() {
+  let { coords, values } = getCoordsAndValue();
+  let color = calculateColor(values.slice(-1));
+  const mapStyles = {};
+
+  return (
+    <Map
+      google={window.google}
+      zoom={8}
+      style={mapStyles}
+      initialCenter={{ lat: 47.444, lng: -122.176 }}
+    >
+      <Marker position={{ lat: 48.0, lng: -122.0 }} />
+      <Polyline
+        path={coords}
+        strokeColor={color}
+        strokeOpacity={1}
+        strokeWeight={10}
+      />
+    </Map>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAdnQ_Y4aN6hifZ5BDvDfysv9-jkrt0beE",
+})(CarMap);
+
+// figure out how to update polyline coordinates and color based on new data, i.e. how does it refresh new data?
+
+// poly line with different color gradient to represent another value along car's path
+// add function in here to get data from backend (make new api file similar to graph_api.py)
+// then call lat, lon, and some value from format.json
