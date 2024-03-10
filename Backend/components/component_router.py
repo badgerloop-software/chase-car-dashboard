4e5db9445689a4c05d0770eb3cbdcb6d7bdd5d2c
Mingcan Li <mli727@wisc.edu>
2024-03-10 13:53:42 -0500

    predicting the future

--- Backend/components/component_router.py
+++ Backend/components/component_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from . import graph_api, record_data
+from . import graph_api, record_data, forcast
 
 router = APIRouter()
 
 router.include_router(graph_api.router)
 router.include_router(record_data.router)
+router.include_router(forcast.router)
