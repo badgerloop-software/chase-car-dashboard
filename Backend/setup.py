212697e8508ee6ffbad2679df58b01c6e9b43625
Mingcan Li <mli727@wisc.edu>
2024-04-24 12:51:10 -0500

    increment versions

--- Backend/setup.py
+++ Backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chase-car-dashboard-backend',
-    version='5.1.0',
+    version='5.1.1',
     packages=['core', 'framework', 'components'],
     url='badgersolarracing.org',
     license='',
