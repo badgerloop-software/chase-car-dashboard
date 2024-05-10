bd885924788880feddb0d2ed13b353e69a6d9506
Mingcan Li <mli727@wisc.edu>
2024-05-09 23:58:16 -0500

    show data on google maps, need to implement selector

--- Backend/components/maps_api.py
+++ Backend/components/maps_api.py
@@ -1,54 +1,31 @@
 from core import db
 from fastapi import APIRouter
+import time
 import numpy as np
-import config
 
 router = APIRouter()
 
 @router.get("/maps")
-async def get_maps(data: str, start_time: int, end_time: int):
-    """pass in a comma separated list of data that the frontend requests. start_time and end_time must be in unix milliseconds"""
+async def get_maps(data: str, duration: int = 60):
+    """pass in a datafield to get and return the coordinates with the value of the data of each timestamp"""
     if not data:
-        # use a query to get timestamp
-        query_result = await db.query(['speed'], start_time, end_time, ['avg'],
-                                      (end_time - start_time) // 60 or 1)
-        return {'response': {"timestamps": query_result.index.to_list()}}
-    data = [d.strip() for d in data.split(',')]
-    # sort the data into smaller group by aggregation methods
-    group = {'avg': [], 'first':[], 'last':[], 'max': [], 'min':[], 'no-aggr':[], 'sum': []}
-    for d in data:
-        # TODO change this to actual aggregation specified in data format
-        group['no-aggr'].append(d) if config.FORMAT[d]['type']=='bool' else group['avg'].append(d)
-    result = dict()
-    for key in group:
-        if group[key]:
-            #query the database for each aggregation method
-            query_result = {}
-            if key != 'no-aggr':
-                query_result = await db.query(group[key], start_time, end_time, [key]*len(group[key]), (end_time-start_time)//60 or 1)
-            else:
-                query_result = await db.query_without_aggregation(group[key], start_time, end_time)
-            time_stamp = query_result.index.to_list()
-            # add the tuples to the json
-            for name in group[key]:
-                result[name] = []
-                data_arr = query_result[name].to_list()
-                if key == 'no-aggr': # TODO Replace 'no-aggr' with min-max key
-                    prev_y = -100
-                    prev_y_count = 1
-                    for i in range(len(data_arr)):
-                        # only include the start and end time of a period of same data
-                        if data_arr[i] != prev_y:
-                            if prev_y_count > 1:
-                                result[name].append({'x': time_stamp[i-1], 'y': data_arr[i-1]})
-                            result[name].append({'x': time_stamp[i], 'y': data_arr[i]})
-                            prev_y = data_arr[i]
-                            prev_y_count = 1
-                        elif i == len(data_arr)-1:
-                            result[name].append({'x': time_stamp[i], 'y': data_arr[i]})
-                        else:
-                            prev_y_count += 1
-                else:
-                    result[name] = [{'x': time_stamp[i], 'y': data_arr[i]} for i in range(len(data_arr))]
+        return {"coords": [], "data": []}
+    
+    end_time = int(time.time()*1000)
+    start_time = end_time - duration*1000
+    
+    all_data = await db.query_without_aggregation(["lat", "lon", data], start_time, end_time)
+    
+    # Filter out rows with lat or lon equal to -1000, -1001, or 0
+    all_data = all_data[~((all_data['lat'] == -1000) | 
+                          (all_data['lat'] == -1001) | 
+                          (all_data['lat'] == 0) | 
+                          (all_data['lon'] == -1000) | 
+                          (all_data['lon'] == -1001) | 
+                          (all_data['lon'] == 0))]
+    all_data = all_data.drop_duplicates()
 
-    return {'response': result}
+    return {
+        "coords": [{"lat": d["lat"], "lng": d["lon"]} for _, d in all_data.iterrows()],
+        "data": [d[data] for _, d in all_data.iterrows()]
+    }
--- Frontend/src/Components/GoogleMap/Map.js
+++ Frontend/src/Components/GoogleMap/Map.js
@@ -1,52 +1,118 @@
 import { Map, Marker, Polyline, GoogleApiWrapper } from "google-maps-react";
+import { useCallback } from "react";
+import { useEffect, useMemo, useState } from "react";
+import {useInterval} from "@chakra-ui/react";
+import CONSTANTS from "../../data-constants.json";
 
-let coords = [];
-let values = [];
-
-function getCoordsAndValue() {
-  // call api to get new coords and new other value
-
-  coords.push({ lat: 25.774, lng: -80.19 });
-  coords.push({ lat: 18.466, lng: -66.118 });
-  coords.push({ lat: 32.321, lng: -64.757 });
-  coords.push({ lat: 25.774, lng: -80.19 });
-
-  values.push(100);
-
-  // is this return necessary? CarMap() can just get the coords and values since global variables
-  return coords, values;
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
-function calculateColor(value) {
-  // calculate polyline color based on value
-  return "#000000";
+// https://stackoverflow.com/a/17243070
+const HSVtoRGB = (hsv) => {
+  const [h, s, v] = hsv;
+  let r, g, b, i, f, p, q, t;
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0: r = v; g = t; b = p; break;
+    case 1: r = q; g = v; b = p; break;
+    case 2: r = p; g = v; b = t; break;
+    case 3: r = p; g = q; b = v; break;
+    case 4: r = t; g = p; b = v; break;
+    case 5: r = v; g = p; b = q; break;
+  }
+  let decimal = [
+    Math.round(r * 255),
+    Math.round(g * 255),
+    Math.round(b * 255)
+  ];
+  return "#" + componentToHex(decimal[0]) + componentToHex(decimal[1]) + componentToHex(decimal[2]);
+};
+
+function calculateColor(dataName, value) {
+  // calculate the color value for each segment of the polyline using the value range from dataformat
+  let min = CONSTANTS[dataName].MIN;
+  let max = CONSTANTS[dataName].MAX;
+  
+  let color = [];
+  for(let i = 0 ; i < value.length - 1; i++) {
+    let mean = (value[i] + value[i + 1])/2;
+    // calculate percentage within the value range
+    let percentage = (mean - min) / (max - min);
+    percentage = Math.max(Math.min(1, percentage), Math.max(0, percentage));
+    // map to hsv from 0 to 120 for transition from green to red
+    console.log(percentage)
+    color.push(HSVtoRGB([percentage*0.375, 1, 1]))
+  }
+  return color;
 }
 
 function CarMap() {
-  let { coords, values } = getCoordsAndValue();
-  let color = calculateColor(values.slice(-1));
-  const mapStyles = {};
+  const [mapPath, setMapPath] = useState([]);
+  const [pathColor, setPathColor] = useState([]);
+  const [selectedData, setSelectedData] = useState("speed");
+  const mapStyles = {
+  };
 
-  return (
-    <Map
-      google={window.google}
-      zoom={8}
-      style={mapStyles}
-      initialCenter={{ lat: 47.444, lng: -122.176 }}
-    >
-      <Marker position={{ lat: 48.0, lng: -122.0 }} />
-      <Polyline
-        path={coords}
-        strokeColor={color}
+  const refreshMap = () => {
+    console.log("refreshmap")
+    fetch(`/components/maps?data=${selectedData}&duration=60`)
+    .then(response => {
+      if (response.ok) {
+          return response.json();
+      } else {
+          throw new Error(`Error fetching map with response ${response.status}`);
+      }
+    })
+    .then((body) => {
+        setMapPath(body["coords"])
+        setPathColor(calculateColor("speed", body["data"]))
+    }).catch(error => console.error('Fetch error:', error));
+  }
+
+
+  useInterval(refreshMap, 1000)
+
+  const getLines = () => {
+    let lines = [];
+    for(let i = 0 ; i < mapPath.length - 1; i++) {
+      lines.push(<Polyline
+        path={[mapPath[i], mapPath[i + 1]]}
+        strokeColor={pathColor[i]}
         strokeOpacity={1}
-        strokeWeight={10}
-      />
-    </Map>
-  );
+        strokeWeight={5}
+      />)
+    }
+    return lines
+  }
+
+  const getMap = (mapPath)=>{
+    return (
+      <div style={{position: "absolute", height: '31%', width: '66.6%'}}>
+        <Map
+          google={window.google}
+          zoom={13}
+          style={mapStyles}
+          center={mapPath[mapPath.length-1]}
+          streetViewControl={false}
+          mapType='terrain'
+        >
+          {getLines()}
+        </Map>
+      </div>
+    );
+  }
+
+  return useCallback(getMap(mapPath, pathColor), [mapPath, pathColor])
 }
 
 export default GoogleApiWrapper({
-  apiKey: "API_KEY_HERE",
+  apiKey: "YOUR_API_KEY",
 })(CarMap);
 
 // figure out how to update polyline coordinates and color based on new data, i.e. how does it refresh new data?
